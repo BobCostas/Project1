505c0f32b35d7b9ae7950357586f1de2a1786fe8
Bob Costas <finkzane@gmail.com>
2018-01-25 12:46:36 -0700

    Added testing for number zero

--- src/p1_Package/DigitClassMain.java
+++ src/p1_Package/DigitClassMain.java
@@ -5,9 +5,17 @@ import java.util.Arrays;
 public class DigitClassMain {
     public static void main(String[] args) {
         DigitClass firstDigit = new DigitClass(29, 06, 2);
+
         int decimalAsDigit[] = firstDigit.decToBase(40);
+
         String digitAsBase = Arrays.toString( decimalAsDigit );
+
         System.out.println(firstDigit.getValueAsDecimal());
+
         System.out.println(firstDigit.getValueAsBase());
+
+        DigitClass secondDigit = new DigitClass(0, 7, 2);
+        System.out.println(secondDigit.getValueAsBase());
+        System.out.println(secondDigit.getValueAsDecimal());
     }
 }
