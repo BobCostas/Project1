cfd421b906e890c388226c97f7dc557288854e02
Bob Costas <finkzane@gmail.com>
2018-01-29 15:06:59 -0700

    Testing for add method

--- src/p1_Package/ArithmeticClassMain.java
+++ src/p1_Package/ArithmeticClassMain.java
@@ -1,4 +1,13 @@
 package p1_Package;
 
 public class ArithmeticClassMain {
+    public static void main(String[] args ) {
+        DigitClass firstDigit = new DigitClass(2, 10, 10);
+        DigitClass secondDigit = new DigitClass(2, 10, 10);
+        ArithmeticClass arithmeticClass = new ArithmeticClass();
+        DigitClass sum = arithmeticClass.addRegisters(firstDigit, secondDigit);
+
+        System.out.println(sum.getValueAsDecimal());
+        System.out.println(sum.getValueAsBase());
+    }
 }
