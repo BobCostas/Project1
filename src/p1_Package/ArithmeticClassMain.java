74dc4e24ff9ddecc08e4ecc5ea4f1c1bcf7f42a5
Bob Costas <finkzane@gmail.com>
2018-01-30 12:14:47 -0700

    Added testing for multiplyRegisters

--- src/p1_Package/ArithmeticClassMain.java
+++ src/p1_Package/ArithmeticClassMain.java
@@ -2,19 +2,20 @@ package p1_Package;
 
 public class ArithmeticClassMain {
     public static void main(String[] args ) {
-        DigitClass firstDigit = new DigitClass(2, 10, 19);
-        DigitClass secondDigit = new DigitClass(2, 10, 10);
+        DigitClass firstDigit = new DigitClass(3, 100, 23);
+        DigitClass secondDigit = new DigitClass(3, 100,56 );
         ArithmeticClass arithmeticClass = new ArithmeticClass();
         DigitClass sum = arithmeticClass.addRegisters(firstDigit, secondDigit);
         DigitClass difference = arithmeticClass.subtractRegisters(firstDigit, secondDigit);
 
 //        System.out.println(sum.getValueAsBase());
-//        System.out.println(sum.getValueAsDecimal());
+        System.out.println(sum.getValueAsDecimal());
 //        System.out.println(sum.numDigits);
 
 
-        System.out.println(difference);
-        System.out.println(difference.getValueAsBase());
-        System.out.println(difference.getValueAsDecimal());
+//        System.out.println(difference.getValueAsBase());
+//        System.out.println(difference.getValueAsDecimal());
+            System.out.println( arithmeticClass.multiplyRegisters(firstDigit, secondDigit).getValueAsDecimal());
+
     }
 }
