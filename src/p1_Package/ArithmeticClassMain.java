02858757ee22720c036514abd928184106a01fc0
Bob Costas <finkzane@gmail.com>
2018-01-29 20:43:06 -0700

    Added testing for subtractRegisters

--- src/p1_Package/ArithmeticClassMain.java
+++ src/p1_Package/ArithmeticClassMain.java
@@ -2,12 +2,19 @@ package p1_Package;
 
 public class ArithmeticClassMain {
     public static void main(String[] args ) {
-        DigitClass firstDigit = new DigitClass(2, 10, 10);
+        DigitClass firstDigit = new DigitClass(2, 10, 19);
         DigitClass secondDigit = new DigitClass(2, 10, 10);
         ArithmeticClass arithmeticClass = new ArithmeticClass();
         DigitClass sum = arithmeticClass.addRegisters(firstDigit, secondDigit);
+        DigitClass difference = arithmeticClass.subtractRegisters(firstDigit, secondDigit);
 
-        System.out.println(sum.getValueAsDecimal());
-        System.out.println(sum.getValueAsBase());
+//        System.out.println(sum.getValueAsBase());
+//        System.out.println(sum.getValueAsDecimal());
+//        System.out.println(sum.numDigits);
+
+
+        System.out.println(difference);
+        System.out.println(difference.getValueAsBase());
+        System.out.println(difference.getValueAsDecimal());
     }
 }
